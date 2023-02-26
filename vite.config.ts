47332cdbe24a59a9531be4223b0734b80a860ab6
Gabriel <gabrielgcr45@gmail.com>
2023-02-25 21:46:40 -0300

    aaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [solid({ adapter: vercel({}) })],
-  optimizeDeps: {
-    esbuildOptions: { target: 'esnext' },
-  },
 })
